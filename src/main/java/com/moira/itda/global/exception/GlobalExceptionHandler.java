fdf0101d5226c79f8b38314e8861045a6aadd89e
hunzzzzz <hun819.st@gmail.com>
2025-12-03 21:14:50 +0900

    GlobalExceptionHandler 오류 수정

--- src/main/java/com/moira/itda/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/moira/itda/global/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.moira.itda.global.exception;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -8,11 +9,15 @@ import java.time.ZonedDateTime;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(ItdaException.class)
-    public ErrorResponse handleItdaException(ItdaException e) {
-        return ErrorResponse.builder()
+    public ResponseEntity<ErrorResponse> handleItdaException(ItdaException e) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
                 .errorCode(e.getErrorCode())
                 .message(e.getMessage())
                 .time(ZonedDateTime.now())
                 .build();
+
+        return ResponseEntity
+                .status(e.getErrorCode().getHttpStatus())
+                .body(errorResponse);
     }
 }
