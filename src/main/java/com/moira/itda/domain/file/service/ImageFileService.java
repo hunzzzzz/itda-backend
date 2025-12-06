c54875ce6e18cc2b2069e54c292927f53cc0d2d2
hunzzzzz <hun819.st@gmail.com>
2025-12-06 19:07:28 +0900

    이미지 파일 업로드 기능 수정

--- src/main/java/com/moira/itda/domain/file/service/ImageFileService.java
+++ src/main/java/com/moira/itda/domain/file/service/ImageFileService.java
@@ -4,11 +4,10 @@ import com.moira.itda.domain.file.dto.request.ImageFileRequest;
 import com.moira.itda.domain.file.dto.response.FileIdResponse;
 import com.moira.itda.domain.file.mapper.ImageFileMapper;
 import com.moira.itda.global.entity.ImageFile;
+import com.moira.itda.global.file.AwsS3Handler;
 import com.moira.itda.global.file.FileInfo;
 import com.moira.itda.global.file.FileValidator;
-import com.moira.itda.global.file.LocalFileStorageHandler;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,19 +18,15 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @Service
 public class ImageFileService {
-    @Value("${spring.profiles.active}")
-    private String profilesActive;
-
+    private final AwsS3Handler awsS3Handler;
     private final FileValidator fileValidator;
     private final ImageFileMapper imageFileMapper;
-    private final LocalFileStorageHandler localFileStorageHandler;
 
     /**
      * 이미지 파일 저장
      */
     @Transactional
     public FileIdResponse uploadFile(ImageFileRequest request) {
-        FileInfo fileInfo = null;
         String fileId = UUID.randomUUID().toString();
 
         // [1] 유효성 검사
@@ -40,12 +35,7 @@ public class ImageFileService {
         // [2] 이미지 파일 저장 (파일 시스템)
         for (int i = 0; i < request.files().size(); i++) {
             MultipartFile file = request.files().get(i);
-
-            if ("local".equals(profilesActive)) {
-                fileInfo = localFileStorageHandler.saveFile(file, fileId, i + 1, request.identifier());
-            } else if ("prod".equals(profilesActive)) {
-                // TODO: AWS S3 로직 구현
-            }
+            FileInfo fileInfo = awsS3Handler.upload(file, fileId, i + 1, request.identifier());
 
             // [3] 이미지 파일 저장 (DB)
             if (fileInfo != null) {
