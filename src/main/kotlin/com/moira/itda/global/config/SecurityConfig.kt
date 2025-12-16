bc81bc82dec7f14c1db222932c311e4543fd8ae1
hunzzzzz <hun819.st@gmail.com>
2025-12-16 17:40:20 +0900

    WebConfig, SecurityConfig 초안 작성

---
+++ src/main/kotlin/com/moira/itda/global/config/SecurityConfig.kt
@@ -0,0 +1,53 @@
+package com.moira.itda.global.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class SecurityConfig {
+    /**
+     * PasswordEncoder Bean 등록
+     */
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+
+    /**
+     * AuthenticationManager Bean 등록
+     */
+    @Bean
+    fun authenticationManager(authenticationConfiguration: AuthenticationConfiguration): AuthenticationManager =
+        authenticationConfiguration.authenticationManager
+
+    /**
+     * 스프링 시큐리티 필터 체인 정의
+     */
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            // [1] CSRF, FormLogin, Basic 인증 비활성화
+            .csrf { it.disable() }
+            .cors { }
+            .formLogin { it.disable() }
+            .httpBasic { it.disable() }
+            // [2] 세션 관리: STATELESS (JWT 기반)
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            // [3] 인가 규칙 설정
+            .authorizeHttpRequests {
+                it
+                    .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                    .requestMatchers(HttpMethod.GET, "/api/signup/**").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/api/signup/**").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/api/login/**").permitAll()
+                    .anyRequest().authenticated()
+            }
+            .build()
+    }
+}
