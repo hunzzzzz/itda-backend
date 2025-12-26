790c8416c15c9c13c509f46a18e8e716938bf6b1
hunzzzzz <hun819.sc@gmail.com>
2025-12-26 11:27:17 +0900

    채팅방 거래 목록 조회 기능 구현

--- src/main/kotlin/com/moira/itda/domain/user/mychat/dto/response/TradeSuggestResponse.kt
+++ src/main/kotlin/com/moira/itda/domain/user/mychat/dto/response/TradeSuggestResponse.kt
@@ -6,8 +6,12 @@ data class TradeSuggestResponse(
     val tradeStatus: String,
     val tradeType: String,
     val tradeTitle: String,
+    val sellerId: String,
+    val sellerNickname: String,
+    val buyerId: String,
+    val buyerNickname: String,
     // 제안 관련 정보
-    val suggestId: Long,
+    val suggestId: String,
     val suggestStatus: String,
     val purchaseItemId: Long?,
     val purchaseItemName: String?,
