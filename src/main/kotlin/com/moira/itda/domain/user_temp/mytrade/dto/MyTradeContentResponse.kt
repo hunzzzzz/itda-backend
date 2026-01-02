835a4596d7e0a6a8f47ca271bce9e3d628b28d90
hunzzzzz <hun819.sc@gmail.com>
2026-01-02 13:13:18 +0900

    불필요 파일 제거

--- src/main/kotlin/com/moira/itda/domain/user_temp/mytrade/dto/MyTradeContentResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.moira.itda.domain.user_temp.mytrade.dto
-
-data class MyTradeContentResponse(
-    val trade: MyTradeResponse,
-    val items: List<MyTradeItemResponse>
-)
--- src/main/kotlin/com/moira/itda/domain/user_temp/mytrade/dto/MyTradePageResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.moira.itda.domain.user_temp.mytrade.dto
-
-import com.moira.itda.global.pagination.dto.PageResponse
-
-data class MyTradePageResponse(
-    val content: List<MyTradeContentResponse>,
-    val page: PageResponse
-)
