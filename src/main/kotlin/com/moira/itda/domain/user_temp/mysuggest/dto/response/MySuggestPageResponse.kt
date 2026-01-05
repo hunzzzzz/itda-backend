03a8831cb11cfde3a9b4e1ce863b50775710c830
hunzzzzz <hun819.sc@gmail.com>
2026-01-05 14:59:01 +0900

    미사용 코드 제거

--- src/main/kotlin/com/moira/itda/domain/user_temp/mysuggest/dto/response/MySuggestPageResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.moira.itda.domain.user_temp.mysuggest.dto.response
-
-import com.moira.itda.domain.suggest.dto.response.MyTradeSuggestResponse
-import com.moira.itda.global.pagination.dto.PageResponse
-
-data class MySuggestPageResponse(
-    val suggest: List<MyTradeSuggestResponse>,
-    val page: PageResponse
-)
