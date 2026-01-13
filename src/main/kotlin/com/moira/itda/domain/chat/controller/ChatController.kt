3a3cd40a599bb2a3336b07da64c4bbd4924e3dfa
hunzzzzz <hun819.sc@gmail.com>
2026-01-13 15:19:30 +0900

    채팅 전송 컨트롤러 수정

--- src/main/kotlin/com/moira/itda/domain/chat/controller/ChatController.kt
+++ src/main/kotlin/com/moira/itda/domain/chat/controller/ChatController.kt
@@ -12,8 +12,9 @@ import com.moira.itda.global.auth.dto.UserAuth
 import org.springframework.http.ResponseEntity
 import org.springframework.messaging.handler.annotation.DestinationVariable
 import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.handler.annotation.SendTo
+import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.*
-import java.security.Principal
 
 @RestController
 class ChatController(
@@ -56,13 +57,16 @@ class ChatController(
      * 내 활동 > 채팅 > 채팅방 > 메시지 전송
      */
     @MessageMapping("/chat/{chatRoomId}/message")
+    @SendTo("/sub/")
     fun sendMessage(
         @DestinationVariable chatRoomId: String,
         request: ChatMessageRequest,
-        userPrincipal: Principal
+        authentication: Authentication
     ) {
-        println("====== ${userPrincipal.name} =====")
-        chatService.sendMessage(senderId = userPrincipal.name, chatRoomId = chatRoomId, request = request)
+        val userAuth = authentication.principal as UserAuth
+
+        println("====== $userAuth =====")
+        chatService.sendMessage(senderId = userAuth.userId, chatRoomId = chatRoomId, request = request)
     }
 
     /**
