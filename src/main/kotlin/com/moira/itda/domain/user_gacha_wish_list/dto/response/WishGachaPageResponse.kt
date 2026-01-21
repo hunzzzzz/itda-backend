c1c67b3db24a10b4ce22de96dce090922ff6c569
hunzzzzz <hun819.sc@gmail.com>
2026-01-21 16:35:20 +0900

    미사용코드 제거

--- src/main/kotlin/com/moira/itda/domain/user_gacha_wish_list/dto/response/WishGachaPageResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.moira.itda.domain.user_gacha_wish_list.dto.response
-
-import com.moira.itda.global.pagination.dto.PageResponse
-
-data class WishGachaPageResponse(
-    val content: List<WishGachaResponse>,
-    val page: PageResponse
-)
